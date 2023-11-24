c1d6c1d48900de8bcb80572b289805c24977e225
Luciana Xarim <lxxmendes@gmail.com>
2023-11-24 14:20:11 -0300

    adicionando subscriptionQueue

--- src/main/java/ada/tech/tenthirty/tvpackages/config/WebClientConfig.java
+++ src/main/java/ada/tech/tenthirty/tvpackages/config/WebClientConfig.java
@@ -1,5 +1,8 @@
 package ada.tech.tenthirty.tvpackages.config;
+<<<<<<< HEAD
 
+=======
+>>>>>>> 2c95300 (adicionando subscriptionQueue)
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +10,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 
 @Configuration
+<<<<<<< HEAD
 public class WebClientConfig<StockClient> {
   @Value("${config.technical-visit.url}")
   private String technicalVisitUrl;
@@ -17,3 +21,15 @@ public class WebClientConfig<StockClient> {
             .build();
   }
 }
+=======
+public class WebClientConfig<StockClient>  {
+    @Value("${config.subscription-package.url}")
+    private String subscriptionUrl;
+
+    @Bean
+    WebClient webClient(){
+        return WebClient.builder().baseUrl(subscriptionUrl)
+                .build();
+    }
+}
+>>>>>>> 2c95300 (adicionando subscriptionQueue)
