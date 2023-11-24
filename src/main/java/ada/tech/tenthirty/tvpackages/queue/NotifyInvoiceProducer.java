8bc6e2afe2b395da45f360c8a6c56962aaeac3ee
Ar3secchim <renarasecchim@gmail.com>
2023-11-24 19:28:55 -0300

    feat: update rebase development

--- src/main/java/ada/tech/tenthirty/tvpackages/queue/NotifyInvoiceProducer.java
+++ src/main/java/ada/tech/tenthirty/tvpackages/queue/NotifyInvoiceProducer.java
@@ -6,17 +6,17 @@ import org.springframework.amqp.core.Queue;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import ada.tech.tenthirty.tvpackages.queue.payloads.SubscriptionRequest;
+import ada.tech.tenthirty.tvpackages.queue.payloads.NotifyInvoiceRequest;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class SubscriptionProducer {
+public class NotifyInvoiceProducer {
     private final RabbitTemplate rabbitTemplate;
     private final Queue queue;
     private final ObjectMapper objectMapper;
 
-    public void execute (SubscriptionRequest SubscriptionPackage) throws JsonProcessingException {
+    public void execute (NotifyInvoiceRequest SubscriptionPackage) throws JsonProcessingException {
         String message = objectMapper.writeValueAsString(SubscriptionPackage);
         rabbitTemplate.convertSendAndReceive(queue.getName(), message);
     }
---
+++ src/main/java/ada/tech/tenthirty/tvpackages/queue/payloads/NotifyInvoiceRequest.java
@@ -0,0 +1,13 @@
+package ada.tech.tenthirty.tvpackages.queue.payloads;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class NotifyInvoiceRequest {
+    private String idUser;
+    private LocalDateTime issueDate;
+    private List<Package> listPackage;
+    //Lista com [{"skuid": "string";},{"skuid": "string";}]
+}
